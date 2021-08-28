0c5bac0f7e296611b37579fcb69f7ad6a36a6305
Carlos Eduardo <ferreiira.kadu@gmail.com>
2021-08-28 19:36:20 -0300

    adicionando funções de editar e dar like nos tweets

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,8 @@
 class Tweet < ApplicationRecord
+  validates_presence_of :body
+  validates_length_of :body, minimum: 3
+
+  after_create_commit { broadcast_prepend_to :tweets }
+  after_update_commit { broadcast_replace_to :tweets }
+  after_destroy_commit { broadcast_remove_to :tweets }
 end
